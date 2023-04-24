b24e018b9f4d8e9bcc65a56a5d33ba2a9fa69b6c
Fred Jiang <jiangf@shinetechsoftware.com>
2023-04-24 16:14:01 +0800

    code clean

--- Mosh/docker-compose/frontend/src/services/api.js
+++ Mosh/docker-compose/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:5001/api";
+const baseUrl = process.env.API_URL || "http://localhost:5001/api";
 
 const api = {
   get: (endpoint) => axios.get(baseUrl + endpoint),
